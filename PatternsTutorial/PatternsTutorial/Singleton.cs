3c43ae29889d2f19e54ef6a5096b4f980423d2a8
kapeway <kavinfranco19@gmail.com>
2016-04-10 16:22:20 -0400

    Singleton Pattern

---
+++ PatternsTutorial/PatternsTutorial/Singleton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PatternsTutorial
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            if(_instance==null)
+                _instance=new Singleton();
+            return _instance;
+        }
+    }
+
+    public class BasicLoadBalancer
+    {
+        private static BasicLoadBalancer _instance;
+        private List<string> _servers=new List<string>();
+        private Random _random =new Random();
+
+        private static object syncLock= new object();
+
+        protected BasicLoadBalancer()
+        {
+            _servers.Add("server1");
+            _servers.Add("server2");
+            _servers.Add("server3");
+            _servers.Add("server4");
+        }
+
+        public static BasicLoadBalancer GetLoadBalancer()
+        {
+            if (_instance == null)
+            {
+                lock (syncLock)
+                {
+                    _instance=new BasicLoadBalancer();
+                }
+            }
+
+            return _instance;
+        }
+
+        public string Server
+        {
+            get
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r];
+            }
+        }
+    }
+}
---
+++ PatternsTutorial/PatternsTutorialTests/SingletonTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using PatternsTutorial;
+
+namespace PatternsTutorialTests
+{
+    [TestFixture]
+    public class SingletonTests
+    {
+        [Test]
+        public void CreationTest()
+        {
+            var instance1 = Singleton.Instance();
+            var instance2 = Singleton.Instance();
+            Assert.That(instance1,Is.SameAs(instance2));
+        }
+
+        [Test]
+        public void BasicLoadBalancerCreationTest()
+        {
+            for (var i = 0; i < 6; i++)
+            {
+                var server1=BasicLoadBalancer.GetLoadBalancer();
+                Console.WriteLine(server1.Server);
+
+            }
+        }
+    }
+}
